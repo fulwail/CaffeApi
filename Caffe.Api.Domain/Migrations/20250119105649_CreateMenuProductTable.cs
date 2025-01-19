00997726fbec95035d1a2bbfe602989027ef94a8
fulwail <kiry956@gmail.com>
2025-01-19 16:04:18 +0500

    Создал миграции для создания таблиц заказов и продуктов из меню

---
+++ Caffe.Api.Domain/Migrations/20250119105649_CreateMenuProductTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Caffe.Api.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateMenuProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MenuProducts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuProducts", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuProducts_Name",
+                table: "MenuProducts",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MenuProducts");
+        }
+    }
+}
---
+++ Caffe.Api.Domain/Migrations/20250119105828_CreateOrdersTable.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Caffe.Api.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateOrdersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "OrderId",
+                table: "MenuProducts",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    VisitorName = table.Column<string>(type: "text", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PaymentType = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuProducts_OrderId",
+                table: "MenuProducts",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MenuProducts_Orders_OrderId",
+                table: "MenuProducts",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MenuProducts_Orders_OrderId",
+                table: "MenuProducts");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MenuProducts_OrderId",
+                table: "MenuProducts");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "MenuProducts");
+        }
+    }
+}
